1b3eaa590fd9863890bb3d78ce9c1365f69395e5
BaldwinTong <765160998@qq.com>
2021-10-13 17:33:34 +0800

    宿舍模块功能开发

--- src/views/HRsystem/dormitoryMange.vue
+++ src/views/HRsystem/dormitoryMange.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <div class="tabs"></div>
     <div class="box">
       <div class="tree-area">
         <el-tree
@@ -13,102 +12,274 @@
         >
         </el-tree>
       </div>
-      <div class="detail">
-        <div class="detail-card" v-if="id == 1">
-          <h1>宿舍资料</h1>
-          <el-form ref="form" :model="buildingForm" label-width="120px">
-            <el-form-item label="宿舍大楼号：" style="width: 420px">
-              <el-input v-model="buildingForm.BuildingNum"></el-input>
-            </el-form-item>
-            <el-form-item label="宿舍大楼名：" style="width: 420px">
-              <el-input v-model="buildingForm.BuildingName"></el-input>
-            </el-form-item>
-            <el-form-item label="备注：" style="width: 420px">
-              <el-input v-model="buildingForm.dec" type="textarea"></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
-        <div class="detail-card" v-if="id == 2">
-          <h1>房间资料</h1>
-          <el-form ref="form" :model="roomForm" label-width="120px">
-            <el-form-item label="房间编号：" style="width: 420px">
-              <el-input v-model="roomForm.roomNum"></el-input>
-            </el-form-item>
-            <el-form-item label="房间名称" style="width: 420px">
-              <el-input v-model="roomForm.roomName"></el-input>
-            </el-form-item>
-            <el-form-item label="床位数" style="width: 420px">
-              <el-input v-model="roomForm.bedNum"></el-input>
-            </el-form-item>
-            <el-form-item label="应住人数" style="width: 420px">
-              <el-input v-model="roomForm.setNum"></el-input>
-            </el-form-item>
-            <el-form-item label="住宿性别" style="width: 420px">
-              <el-input v-model="roomForm.setGrender"></el-input>
-            </el-form-item>
-            <el-form-item label="住宿类型" style="width: 420px">
-              <el-input v-model="roomForm.setType"></el-input>
-            </el-form-item>
-            <el-form-item label="备注" style="width: 420px">
-              <el-input v-model="roomForm.desc" type="textarea"></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
-        <div class="detail-card" v-if="id == 3">
-          <h1>床位资料</h1>
-          <el-form
-            ref="form"
-            :inline="true"
-            :model="bedForm"
-            label-width="120px"
-          >
-            <div>
-              <h5>床位资料</h5>
-              <el-form-item label="床位号" style="width: 420px">
-                <el-input v-model="bedForm.bedNum"></el-input>
-              </el-form-item>
-              <el-form-item label="是否空置" style="width: 420px">
-                <el-radio-group v-model="radio">
-                  <el-radio :label="1">是</el-radio>
-                  <el-radio :label="2">否</el-radio>
-                </el-radio-group>
-              </el-form-item>
-              <el-form-item label="宿舍按金" style="width: 420px">
-                <el-input v-model="bedForm.deposit"></el-input>
-              </el-form-item>
-              <el-form-item label="房租" style="width: 420px">
-                <el-input v-model="bedForm.rent"></el-input>
-              </el-form-item>
-              <el-form-item label="备注" style="width: 420px">
-                <el-input v-model="bedForm.desc" type="textarea"></el-input>
-              </el-form-item>
+
+      <div class="data-right">
+        <el-tabs
+          v-model="activeName"
+          type="border-card"
+          @tab-click="handleClick"
+        >
+          <el-tab-pane label="基本资料" name="1">
+            <div class="detail">
+              <div class="detail-card" v-if="id < 10 && id >= 1">
+                <h1>宿舍资料</h1>
+                <el-form ref="form" :model="buildingForm" label-width="120px">
+                  <el-form-item label="宿舍大楼号：">
+                    <el-input v-model="buildingForm.BuildingNum"></el-input>
+                  </el-form-item>
+                  <el-form-item label="宿舍大楼名：">
+                    <el-input v-model="buildingForm.BuildingName"></el-input>
+                  </el-form-item>
+                  <el-form-item label="备注：">
+                    <el-input
+                      v-model="buildingForm.dec"
+                      type="textarea"
+                    ></el-input>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <div class="detail-card" v-if="id > 10 && id < 100">
+                <h1>房间资料</h1>
+                <el-form
+                  ref="form"
+                  :inline="true"
+                  :model="roomForm"
+                  label-width="120px"
+                >
+                  <el-form-item label="房间编号：">
+                    <el-input v-model="roomForm.roomNum"></el-input>
+                  </el-form-item>
+                  <el-form-item label="房间名称">
+                    <el-input v-model="roomForm.roomName"></el-input>
+                  </el-form-item>
+                  <el-form-item label="床位数">
+                    <el-input v-model="roomForm.bedNum"></el-input>
+                  </el-form-item>
+                  <el-form-item label="应住人数">
+                    <el-input v-model="roomForm.setNum"></el-input>
+                  </el-form-item>
+                  <el-form-item label="住宿性别">
+                    <el-input v-model="roomForm.setGrender"></el-input>
+                  </el-form-item>
+                  <el-form-item label="住宿类型">
+                    <el-input v-model="roomForm.setType"></el-input>
+                  </el-form-item>
+                  <el-form-item label="备注">
+                    <el-input
+                      v-model="roomForm.desc"
+                      type="textarea"
+                    ></el-input>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <div class="detail-card" v-if="id > 100 && id < 1000">
+                <h1>床位资料</h1>
+                <el-form
+                  ref="form"
+                  :inline="true"
+                  :model="bedForm"
+                  label-width="120px"
+                >
+                  <div>
+                    <h5>床位资料</h5>
+                    <el-form-item label="床位号">
+                      <el-input v-model="bedForm.bedNum"></el-input>
+                    </el-form-item>
+                    <el-form-item label="是否空置">
+                      <el-radio-group v-model="radio">
+                        <el-radio :label="1">是</el-radio>
+                        <el-radio :label="2">否</el-radio>
+                      </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="宿舍按金">
+                      <el-input v-model="bedForm.deposit"></el-input>
+                    </el-form-item>
+                    <el-form-item label="房租">
+                      <el-input v-model="bedForm.rent"></el-input>
+                    </el-form-item>
+                    <el-form-item label="备注">
+                      <el-input
+                        class="stretchNone"
+                        v-model="bedForm.desc"
+                        type="textarea"
+                        maxlength="60"
+                        style="width: 447px"
+                      ></el-input>
+                    </el-form-item>
+                  </div>
+                  <div>
+                    <h5>入住人员资料</h5>
+                    <el-form-item label="住宿证号">
+                      <el-input v-model="bedForm.getInId"></el-input>
+                    </el-form-item>
+                    <el-form-item label="入住日期">
+                      <el-input v-model="bedForm.getInDate"></el-input>
+                    </el-form-item>
+                    <el-form-item label="编号">
+                      <el-input v-model="bedForm.companyId"></el-input>
+                    </el-form-item>
+                    <el-form-item label="姓名">
+                      <el-input v-model="bedForm.userName"></el-input>
+                    </el-form-item>
+                    <el-form-item label="部门编号">
+                      <el-input v-model="bedForm.departmentId"></el-input>
+                    </el-form-item>
+                    <el-form-item label="部门名称">
+                      <el-input v-model="bedForm.departmentName"></el-input>
+                    </el-form-item>
+                    <el-form-item label="学历">
+                      <el-input v-model="bedForm.qualifications"></el-input>
+                    </el-form-item>
+                  </div>
+                </el-form>
+              </div>
             </div>
-            <div>
-              <h5>入住人员资料</h5>
-              <el-form-item label="住宿证号" style="width: 420px">
-                <el-input v-model="bedForm.getInId"></el-input>
-              </el-form-item>
-              <el-form-item label="入住日期" style="width: 420px">
-                <el-input v-model="bedForm.getInDate"></el-input>
-              </el-form-item>
-              <el-form-item label="编号" style="width: 420px">
-                <el-input v-model="bedForm.companyId"></el-input>
-              </el-form-item>
-              <el-form-item label="姓名" style="width: 420px">
-                <el-input v-model="bedForm.userName"></el-input>
-              </el-form-item>
-              <el-form-item label="部门编号" style="width: 420px">
-                <el-input v-model="bedForm.departmentId"></el-input>
-              </el-form-item>
-              <el-form-item label="部门名称" style="width: 420px">
-                <el-input v-model="bedForm.departmentName"></el-input>
-              </el-form-item>
-              <el-form-item label="学历" style="width: 420px">
-                <el-input v-model="bedForm.qualifications"></el-input>
-              </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="空床情况" name="2">
+            <div class="EmptybedData">
+              <div class="emptySearch">
+                <el-input type="text" v-model="emptyValue" clearable>
+                  <i
+                    slot="prefix"
+                    class="el-input__icon el-icon-search"
+                    style="color: #ccc"
+                  ></i>
+                </el-input>
+                <el-button type="primary" class="emptyBtn">搜索</el-button>
+              </div>
+              <el-table :data="emptyBed" border>
+                <el-table-column
+                  type="index"
+                  label="序号"
+                  width="50px"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column prop="dormiNum" label="宿舍号" align="center">
+                </el-table-column>
+                <el-table-column prop="roomNum" label="房间号" align="center">
+                </el-table-column>
+                <el-table-column prop="bedNum" label="床位号" align="center">
+                </el-table-column>
+                <el-table-column prop="desc" label="备注" align="center">
+                </el-table-column>
+              </el-table>
             </div>
-          </el-form>
-        </div>
+          </el-tab-pane>
+          <el-tab-pane label="人事资料" name="3">
+            <div class="emptySearch">
+              <el-input type="text" v-model="peopleInfoValue" clearable>
+                <i
+                  slot="prefix"
+                  class="el-input__icon el-icon-search"
+                  style="color: #ccc"
+                ></i>
+              </el-input>
+              <el-button type="primary" class="emptyBtn">搜索</el-button>
+            </div>
+            <div class="table">
+              <el-table :data="tableData" border>
+                <el-table-column
+                  type="index"
+                  label="序号"
+                  width="60px"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="dormitoryNum"
+                  label="宿舍号"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column prop="roomNum" label="房间号" align="center">
+                </el-table-column>
+                <el-table-column prop="bedNum" label="床位号" align="center">
+                </el-table-column>
+                <el-table-column prop="userNumber" label="编号" align="center">
+                </el-table-column>
+                <el-table-column prop="userName" label="姓名" align="center">
+                </el-table-column>
+                <el-table-column
+                  prop="departmentNum"
+                  label="部门号码"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="departmentName"
+                  label="部门名称"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="getInDate"
+                  label="入住日期"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column prop="desc" label="备注" align="center">
+                </el-table-column>
+              </el-table>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="区域查询" name="4">
+            <div class="emptySearch">
+              <el-input type="text" v-model="domainValue" clearable>
+                <i
+                  slot="prefix"
+                  class="el-input__icon el-icon-search"
+                  style="color: #ccc"
+                ></i>
+              </el-input>
+              <el-button type="primary" class="emptyBtn">搜索</el-button>
+            </div>
+            <div class="table">
+              <el-table :data="domainTableData" border>
+                <el-table-column
+                  type="index"
+                  label="序号"
+                  width="60px"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="domainNum"
+                  label="区域号"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column prop="roomNum" label="房间号" align="center">
+                </el-table-column>
+                <el-table-column prop="roomName" label="房间名称" align="center">
+                </el-table-column>
+                <el-table-column prop="bedNum" label="床位数" align="center">
+                </el-table-column>
+                <el-table-column prop="shouldBe" label="应住数" align="center">
+                </el-table-column>
+                <el-table-column
+                  prop="gender"
+                  label="性别"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="typeof"
+                  label="类别"
+                  align="center"
+                >
+                </el-table-column>
+                <el-table-column prop="desc" label="备注" align="center">
+                </el-table-column>
+              </el-table>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="更多操作" name="5">
+            <el-button type="primary" around>增加宿舍</el-button>
+            <el-button type="primary" around>增加房间</el-button>
+            <el-button type="primary" around>增加床位</el-button>
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </div>
   </div>
@@ -118,10 +289,46 @@
 export default {
   data() {
     return {
-      id: 3,
+      activeName: "1",
+      emptyValue: "",
+      emptyBed: [
+        {
+          dormiNum: "001",
+          roomNum: "001",
+          bedNum: "001",
+          desc: "",
+        },
+      ],
+      peopleInfoValue: "",
+      domainValue:'',
+      domainTableData:[{
+        domainNum:'02',
+        roomNum:'A208',
+        roomName:'A208',
+        bedNum:'4',
+        shouldBe:'4',
+        gender:'女',
+        typeof:'',
+        desc:'',
+      }],
+      tableData: [
+        {
+          dormitoryNum: "001",
+          roomNum: "A203",
+          bedNum: "A203-上1",
+          userNumber: "05525",
+          userName: "王大锤",
+          departmentNum: "5626",
+          departmentName: "开发部",
+          getInDate: "2021-10-10",
+          desc: "无",
+        },
+      ],
+      id: 1,
       radio: 2,
       treeData: [
         {
+          id: 10000,
           label: "宿舍资料",
           children: [
             {
@@ -193,35 +400,39 @@ export default {
         deposit: 100,
         rent: 500,
         desc: "",
-        getInId:'0002555',
-        getInDate:'',
-        companyId:'',
-        userName:"",
-        departmentId:"",
-        departmentName:'',
-        qualifications:'',
+        getInId: "0002555",
+        getInDate: "",
+        companyId: "",
+        userName: "",
+        departmentId: "",
+        departmentName: "",
+        qualifications: "",
       },
     };
   },
   methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
     handleNodeClick(data) {
-      if (data.id) {
-        if (data.id < 10) {
-          this.id = 1;
-          data.BuildingName = data.label;
-          this.buildingForm = data;
-        } else if (data.id > 10 && data.id < 100) {
-          this.id = 2;
-          console.log(this.id);
-          this.roomForm = data;
-        } else {
-          console.log(212313123);
-          this.id = 2;
-          console.log(this.id);
-          this.roomForm = data;
+      console.log(data);
+      this.id = data.id;
+      if (data.id < 10 && data.id >= 1) {
+        data.BuildingName = data.label;
+        if (this.activeName == 4) {
+          this.domainValue = data.label
         }
-      } else {
-        this.id = 0;
+        this.buildingForm = data;
+      } else if (data.id > 10 && data.id < 100) {
+        this.roomForm = data;
+      } else if (data.id > 100 && data.id < 1000) {
+        this.roomForm = data;
+      }
+
+      if (this.activeName == 2) {
+        this.emptyValue = data.label;
+      } else if (this.activeName == 3) {
+        this.peopleInfoValue = data.label;
       }
     },
   },
@@ -231,13 +442,25 @@ export default {
 };
 </script>
 <style lang="less" scoped>
+.data-right {
+  flex: 1;
+  margin-top: 10px;
+}
+
 .box {
   display: flex;
   justify-content: space-between;
 }
+
+::v-deep.stretchNone {
+  .el-textarea__inner {
+    resize: none;
+  }
+}
+
 .tree-area {
   width: 180px;
-  height: 90vh;
+  min-height: 82vh;
   padding: 4px;
   box-sizing: border-box;
   border: 1px solid slategrey;
@@ -279,18 +502,31 @@ export default {
   background-size: 16px;
 }
 
+// 空床查询
+.emptySearch {
+  width: 30%;
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+  .emptyBtn {
+    margin-left: 10px;
+  }
+}
+
 .detail {
   flex: 1;
-  height: 90vh;
-  margin: 10px 10px 0;
-  border: 1px solid #ccc;
   overflow: auto;
   .detail-card {
-    // text-align: center;
     h1 {
+      width: 100%;
+      text-align: center;
       height: 40px;
       line-height: 40px;
-      margin: 20px 0;
+      margin-bottom: 20px;
+    }
+    h5 {
+      font-size: 14px;
+      margin-bottom: 20px;
     }
   }
 }
